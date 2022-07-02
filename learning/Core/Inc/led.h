c1d6a9e06c127e1d580bd6b36b6b846ea4ba608d
fresh9380 <619645073@qq.com>
2022-07-02 16:35:29 +0800

    structure commit

---
+++ learning/Core/Inc/led.h
@@ -0,0 +1,9 @@
+#ifndef __LED_H__
+#define __LED_H__
+
+#include "main.h"
+
+void MX_GPIO_Init(void);
+
+
+#endif
---
+++ learning/Core/Inc/timer.h
@@ -0,0 +1,9 @@
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+#include "main.h"
+
+void MX_TIM2_Init(void);
+
+
+#endif
